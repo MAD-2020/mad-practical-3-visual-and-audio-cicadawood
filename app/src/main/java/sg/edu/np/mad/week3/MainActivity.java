0374c2a63297cbc579828bd576c58dd66780baf1
Zihui Zeng <s10195353@connect.np.edu.sg>
2020-05-14 23:21:59 +0800

    Practical 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,11 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.widget.VideoView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView view = findViewById(R.id.videoView);
+        view.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.rabbid));
+        view.start();
+        MediaPlayer music = MediaPlayer.create(this,R.raw.baby_laughing);
+        music.start();
+        music.release();
+        music=null;
+
+
+
     }
 }
